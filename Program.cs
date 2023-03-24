3270f72c6717f53566bee16300c1c36f522ac19a
dkrkalic <damirkrkalic11@gmail.com>
2023-03-24 15:43:56 +0100

    app;

--- Program.cs
+++ Program.cs
@@ -5,12 +5,13 @@ TaxCalculator tx = new TaxCalculator();
 
 tx.SetCustomTaxRate(Commodity.Literature, 5);
 tx.SetCustomTaxRate(Commodity.Literature, 6);
-tx.SetCustomTaxRate(Commodity.CulturalServices, 10);
+//tx.SetCustomTaxRate(Commodity.CulturalServices, 10);
 
-//foreach (var kvp in TaxCalculator._customRates)
-//{
-//    Console.WriteLine("Key = [0], Value = {1},  Value = {2}", kvp.Item1, kvp.Item2, kvp.Item3);
-//}
+foreach (var kvp in TaxCalculator._customRates)
+{
+    if(kvp!=null)
+    Console.WriteLine("Key = [0], Value = {1},  Value = {2}", kvp.Item1, kvp.Item2, kvp.Item3);
+}
 
 var taxvalue = tx.GetTaxRateForDateTime(Commodity.Literature, DateTime.Now);
 
